4d73ab6106ed86124a91fe92f4cd47e28f4a2daa
shafiuwu <166884389+shafiuwu@users.noreply.github.com>
2024-10-09 11:40:09 -0600

    login, registro voluntario y perfil funcionales

--- impulsa/src/App.js
+++ impulsa/src/App.js
@@ -4,22 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Login from './pages/Login/Login';
 import Registro from './pages/Registro/Registro';
-import HomePage from './pages/HomePage/HomePage';  // Importa tu nueva página principal
-
+import HomePage from './pages/HomePage/HomePage'; // Importa tu nueva página principal
+import PerfilVoluntario from './pages/Perfil/PerfilVoluntario'; // Importa el componente del perfil del voluntario
 
 function App() {
   return (
     <Router>
-        <Routes>
-          {/* Ruta para la página principal */}
-          <Route path="/" element={<HomePage />} />
-          
-          {/* Ruta para la página de login */}
-          <Route path="/login" element={<Login />} />
+      <Routes>
+        {/* Ruta para la página principal */}
+        <Route path="/" element={<HomePage />} />
+        
+        {/* Ruta para la página de login */}
+        <Route path="/login" element={<Login />} />
 
-          {/* Ruta para la página de registro */}
-          <Route path="/registro" element={<Registro />} />
-        </Routes>
+        {/* Ruta para la página de registro */}
+        <Route path="/registro" element={<Registro />} />
+
+        {/* Ruta para la página del perfil del voluntario */}
+        <Route path="/perfil" element={<PerfilVoluntario />} /> 
+      </Routes>
     </Router>
   );
 }
